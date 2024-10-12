10e48fc993ca050290cfb7ee789b58e8edbd00b5
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-12 23:45:47 +0100

    feat: disable some keybindings by default, and add option to disable highlighting of symbol under cursor

--- lua/instant-lsp/base/lsp.lua
+++ lua/instant-lsp/base/lsp.lua
@@ -160,13 +160,14 @@ return function(custom_opts)
 		config = function(_, opts)
 			vim.diagnostic.config({ virtual_text = not custom_opts.disable_feature.virtual_text })
 			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
 				callback = function(event)
 					setup_lsp_keymaps(custom_opts, event)
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					if not custom_opts.disable_feature.highlight_symbol and client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
 						local highlight_augroup =
-							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+							vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 							buffer = event.buf,
 							group = highlight_augroup,
@@ -180,10 +181,10 @@ return function(custom_opts)
 						})
 
 						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+							group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
 							callback = function(event2)
 								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+								vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
 							end,
 						})
 					end
