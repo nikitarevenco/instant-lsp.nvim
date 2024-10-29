6269ba7b3a769b611e4967d5d74e5d25aab7549b
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-10-29 13:37:11 +0000

    feat: prettier as formatter for yaml files

--- lua/instant-lsp/lang/yaml.lua
+++ lua/instant-lsp/lang/yaml.lua
@@ -1,54 +1,54 @@
 vim.filetype.add({
-	pattern = {
-		[".*/%.github/dependabot.yml"] = "dependabot",
-		[".*/%.github/dependabot.yaml"] = "dependabot",
-		[".*/%.github/workflows[%w/]+.*%.yml"] = "gha",
-		[".*/%.github/workflows/[%w/]+.*%.yaml"] = "gha",
-	},
+  pattern = {
+    [".*/%.github/dependabot.yml"] = "dependabot",
+    [".*/%.github/dependabot.yaml"] = "dependabot",
+    [".*/%.github/workflows[%w/]+.*%.yml"] = "gha",
+    [".*/%.github/workflows/[%w/]+.*%.yaml"] = "gha",
+  },
 })
 
 vim.treesitter.language.register("yaml", "gha")
 vim.treesitter.language.register("yaml", "dependabot")
 
 return function(custom_opts)
-	return {
+  return {
 
-		{
-			"williamboman/mason.nvim",
-			opts = { ensure_installed = { "yamlfmt", "actionlint" } },
-		},
-		{
-			"stevearc/conform.nvim",
-			ft = { "yaml", "gha", "dependabot" },
-			opts = {
-				formatters_by_ft = {
-					yaml = { "yamlfmt" },
-					gha = { "yamlfmt" },
-					dependabot = { "yamlfmt" },
-				},
-			},
-		},
+    {
+      "williamboman/mason.nvim",
+      opts = { ensure_installed = { "prettierd", "actionlint" } },
+    },
+    {
+      "stevearc/conform.nvim",
+      ft = { "yaml", "gha", "dependabot" },
+      opts = {
+        formatters_by_ft = {
+          yaml = { "prettierd" },
+          gha = { "prettierd" },
+          dependabot = { "prettierd" },
+        },
+      },
+    },
 
-		{
-			"mfussenegger/nvim-lint",
-			ft = { "gha" },
-			opts = {
-				linters_by_ft = {
-					gha = { "actionlint" },
-				},
-			},
-		},
+    {
+      "mfussenegger/nvim-lint",
+      ft = { "gha" },
+      opts = {
+        linters_by_ft = {
+          gha = { "actionlint" },
+        },
+      },
+    },
 
-		{
-			"neovim/nvim-lspconfig",
-			ft = { "yaml", "gha", "dependabot" },
-			opts = {
-				servers = {
-					yamlls = {
-						filetypes = { "yaml", "gha", "dependabot" },
-					},
-				},
-			},
-		},
-	}
+    {
+      "neovim/nvim-lspconfig",
+      ft = { "yaml", "gha", "dependabot" },
+      opts = {
+        servers = {
+          yamlls = {
+            filetypes = { "yaml", "gha", "dependabot" },
+          },
+        },
+      },
+    },
+  }
 end
